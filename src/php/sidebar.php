1c0ffc569b66336245bbf9b2523b8523cd41e0e5
Matthias Huttar <mhuttar@olx.com>
2015-05-25 18:13:54 +0200

    adds link network

--- src/php/sidebar.php
+++ src/php/sidebar.php
@@ -38,4 +38,12 @@
     </section>
 
     <?php dynamic_sidebar( 'bakingsheets_sidebar_widget' ); ?>
+
+    <section class="a-teasered menubox l-verticalaligned-item">
+        <h3 class="menubox-head">Links</h3>
+        <p class="menubox-content">
+            <a href="http://www.koestlichundkonsorten.de/" rel="nofollow" target="_blank" title="Köstlich & Konsorten"><img alt="Köstlich & Konsorten" height="121" src="https://lh5.googleusercontent.com/-q_yZL5ls3LQ/Ui4xEcdNpgI/AAAAAAABbNw/treWmHqZIJM/s130/grau.jpg" width="130" /></a>
+        </p>
+    </section>
+
 </div>
